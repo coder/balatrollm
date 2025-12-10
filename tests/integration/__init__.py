455b8d219130689c337662a791882996e781b3b5
S1M0N38 <bertolottosimone@gmail.com>
2025-12-10 18:40:29 +0100

    test(integration): add integration test for BalatroError and BalatroClient

---
+++ tests/integration/__init__.py
@@ -0,0 +1,6 @@
+"""Integration tests for BalatroLLM.
+
+These tests require Balatro to be running with the BalatroBot mod enabled.
+Run with: pytest tests/integration -v
+Skip with: pytest tests/unit -v
+"""
